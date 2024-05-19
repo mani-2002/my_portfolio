a7cec9e542bb9afcf5fda76e3a0c17b5456b901a
mani2002 <manikantavinjamuri8522@gmail.com>
2024-05-19 11:12:14 +0530

    added new project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import project1 from "../../Assets/Projects/project1.png";
+import project2 from "../../Assets/Projects/project2.png";
 
 function Projects() {
   return (
@@ -24,6 +25,18 @@ function Projects() {
               description="This is a Payment Web Application developed using ReactJS + NodeJS + MySQL. With this Payment Web Application a user can signup and login to his/her account and he can link his/her multiple bank accounts to his profile and he/she can select the primary account from the linked accounts and make transactions to the existing users via bank, mobile, upi and made payment transactions are recorded in the transaction history and admin can view his profile and he can manage the users by deleting them."
               ghLink="https://github.com/mani-2002/Manipe"
               demoLink="https://manipe1.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={project2}
+              isBlog={false}
+              title="Book Store Management System"
+              description="A Book Store Management System mainly focuses on CRUD Operations where a user can create a book,
+              delete an existing book, update an existing book details, and view the book details retrieved from the
+              database."
+              ghLink="https://github.com/mani-2002/BSMS_Frontend"
+              demoLink="https://bsms-frontend.vercel.app/"
             />
           </Col>
         </Row>
